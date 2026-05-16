20ccf8e6650b070c82fdcbbec08967cfb8fea5f0
UltiMulti <42481788+UltiMultiGamer@users.noreply.github.com>
2026-05-16 23:02:54 +0300

    for render

--- car-rental/vite.config.js
+++ car-rental/vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	preview: {
+		allowedHosts: ['dryve.onrender.com', '.onrender.com']
+	},
 	ssr: {
 		noExternal: ['bits-ui']
 	}
